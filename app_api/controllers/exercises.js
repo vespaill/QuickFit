e7214217faa8ae9c975fc404747c37c7df3722aa
Victor Espaillat <vicander_99@hotmail.com>
2020-03-28 10:27:39 -0400

    placeholder exercise api functions

--- app_api/controllers/exercises.js
+++ app_api/controllers/exercises.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 const Exercise_model = mongoose.model('Exercise');
 
+/* Placeholder functions to enable the app to start while we figure out how to
+   write the API functionality. */
 const exercisesList = (req, res) => { };
 const exercisesCreate = (req, res) => { };
+const exerciseReadOne = (req, res) => { }
+const exerciseUpdateOne = (req, res) => { }
+const exerciseDeleteOne = (req, res) => { };
 
 module.exports = {
     exercisesList,
-    exercisesCreate
+    exercisesCreate,
+    exerciseReadOne,
+    exerciseUpdateOne,
+    exerciseDeleteOne
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 const ctrlExercises = require('../controllers/exercises');
 
+// Define http requests such as GET and POST
+// For various URLs like /exercises, etc
 router
     .route('/exercises')
     .get(ctrlExercises.exercisesList)
     .post(ctrlExercises.exercisesCreate);
+
+router
+    .route('/exercise/:exerciseid')
+    .get(ctrlExercises.exerciseReadOne)
+    .put(ctrlExercises.exerciseUpdateOne)
+    .delete(ctrlExercises.exerciseDeleteOne);
 
 module.exports = router;
